85dcc83ffcf0dd9ca7f2c53da574225d96ca899b
Nowidas <lukaszkrupczak@gmail.com>
2022-06-25 21:09:14 +0200

    update admin view

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,10 @@
+# Register your models here.
 from django.contrib import admin
 
-# Register your models here.
+from .models import Test, Question, Choice, Test_event, Test_user
+
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Test_event)
+admin.site.register(Test_user)
